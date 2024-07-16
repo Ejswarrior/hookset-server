89ca30f017fe1c7bf65be128f1f5fc6f57f80879
Erik Sabin <eriksabin97@gmail.com>
2024-07-16 07:53:53 -0400

    feat: started jwt auth

---
+++ hookset-server/JWTManager/JWTManager.cs
@@ -0,0 +1,6 @@
+﻿namespace hookset_server.JWTManager
+{
+    public class JWTManager
+    {
+    }
+}
--- hookset-server/Program.cs
+++ hookset-server/Program.cs
@@ -1,20 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.\
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(jwtOption =>
+{
+    var key = builder.Configuration.GetValue<string>("JwtConfig:key");
+    var keyBytes = Encoding.ASCII.GetBytes(key);
+    jwtOption.SaveToken = true;
+    jwtOption.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidateIssuer = true
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
